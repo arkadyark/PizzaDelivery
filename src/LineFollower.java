86fb55dc5f1d8b3a2d2324980836e9f3166b7244
gracekumagai <g.kumagai@mail.utoronto.ca>
2016-11-20 21:13:06 -0500

    Line Follower update!

--- src/LineFollower.java
+++ src/LineFollower.java
@@ -1,3 +1,5 @@
+import lejos.hardware.motor.NXTRegulatedMotor;
+import lejos.hardware.sensor.EV3ColorSensor;
 
 public class LineFollower {
 	/***
@@ -7,14 +9,42 @@ public class LineFollower {
 	 * - Implement interruption support, poll interruptor periodically
 	 */
 
-	
+	private NXTRegulatedMotor leftMotor;
+	private NXTRegulatedMotor rightMotor;
+	private EV3ColorSensor color;
 	private Interruptor interruptor;
+	
+	static float desired = 0.12f;
+	private float kP;
+	private float speed;
 
-	public LineFollower(Interruptor interruptor) {
+	public LineFollower(NXTRegulatedMotor rightMotor, NXTRegulatedMotor leftMotor, EV3ColorSensor color, Interruptor interruptor) {
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		this.color = color;
 		this.interruptor = interruptor;
 	}
 
 	public void driveUntilStopped() {
+		while (!interruptor.isFinished()){
+			float current = getReflectedLight();
+			float error = (desired - current);			
+			float correction = kP*error;
+			
+			leftMotor.setSpeed(Math.round(speed + correction/2.0));
+			leftMotor.forward();
+			rightMotor.setSpeed(Math.round(speed - correction/2.0));
+			rightMotor.forward();
+		}
+		leftMotor.stop();
+		rightMotor.stop();
 	}
+	
+	protected float getReflectedLight() {
+		int sampleSize = color.sampleSize();
+		float[] redsample = new float[sampleSize];
+		color.getRedMode().fetchSample(redsample, 0);
+		return redsample[0];
+	}	
 
 }
